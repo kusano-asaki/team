c94bff68f0796c6d332beda0b8bc936a88046f4a
asaki kusano <darumade4@gmail.com>
2020-08-25 22:33:52 +0900

    user削除をチームオーナーとユーザー自身のみできる/削除ボタン表示制限 修正

--- app/controllers/assigns_controller.rb
+++ app/controllers/assigns_controller.rb
@@ -31,7 +31,7 @@ class AssignsController < ApplicationController
       I18n.t('views.messages.cannot_delete_the_leader')
     elsif Assign.where(user_id: assigned_user.id).count == 1
       I18n.t('views.messages.cannot_delete_only_a_member')
-    elsif assigned_user != current_user && assign.team.owner
+    elsif assigned_user != current_user && assign.team.owner != current_user
       "他のユーザーは削除できません"
     elsif assign.destroy
       set_next_team(assign, assigned_user)
