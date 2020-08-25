cbc3aa81ca7c95cdd0e82f9f11f0f3daaa748e32
asaki kusano <darumade4@gmail.com>
2020-08-25 22:54:07 +0900

    不要コードの削除

--- app/controllers/assigns_controller.rb
+++ app/controllers/assigns_controller.rb
@@ -31,8 +31,6 @@ class AssignsController < ApplicationController
       I18n.t('views.messages.cannot_delete_the_leader')
     elsif Assign.where(user_id: assigned_user.id).count == 1
       I18n.t('views.messages.cannot_delete_only_a_member')
-    elsif assigned_user != current_user && assign.team.owner != current_user
-      "他のユーザーは削除できません"
     elsif assign.destroy
       set_next_team(assign, assigned_user)
       I18n.t('views.messages.delete_member')
