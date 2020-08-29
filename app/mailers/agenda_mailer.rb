dc5500ff6bbe520524535395e58ba6c18ce8debb
asaki kusano <darumade4@gmail.com>
2020-08-29 15:35:42 +0900

    メール通知 修正

--- app/mailers/agenda_mailer.rb
+++ app/mailers/agenda_mailer.rb
@@ -1,7 +1,6 @@
 class AgendaMailer < ApplicationMailer
   def agenda_mail(agenda)
     @agenda = agenda
-    @users = @agenda.team.users
-    mail to: @users.map(&:email), subject: 'アジェンダ削除完了通知'
+    mail to: @agenda.team.members.map(&:email), subject: 'アジェンダ削除完了通知'
   end
 end
